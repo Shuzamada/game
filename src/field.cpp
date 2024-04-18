60dc3f61791e3c2d362dc79469440ca5552807ae
Shuzamada <shuzamada@inbox.ru>
2024-04-18 22:39:02 +0300

    fix move again

--- src/field.cpp
+++ src/field.cpp
@@ -16,36 +16,28 @@ void Field::move(std::string str)
 {
   if (str == "left")
   {
-    for (int i = 0; i < 16; i++)
+    for (int i = 1; i < 16; i++)
     {
       for (int j = 0; j < 20; j++)
       {
-        if (field[i][j][0])
+        if (field[i][j][0] && !field[i-1][j][0])
         {
-          if (i != 0 && !field[i-1][j][0])
-          {
           field[i-1][j][0] = 1;
           field[i][j][0] = 0;
-          }
         }
       }
     }
   }
   else if (str == "right")
   {
-    for (int i = 0; i < 16; i++)
+    for (int i = 14; i >= 0; i--)
     {
-      bool need_break = false;
       for (int j = 0; j < 20; j++)
       {
-        if (field[i][j][0])
+        if (field[i][j][0] && !field[i+1][j][0])
         {
-          if (i != 15 && !field[i+1][j][0])
-          {
-            field[i+1][j][0] = 1;
-            field[i][j][0] = 0;
-            need_break = true;
-          }
+          field[i+1][j][0] = 1;
+          field[i][j][0] = 0;
         }
       }
     }
@@ -54,16 +46,12 @@ void Field::move(std::string str)
   {
     for (int i = 0; i < 16; i++)
     {
-      for (int j = 0; j < 20; j++)
+      for (int j = 18; j >= 0; j--)
       {
-        if (field[i][j][0] == 1)
+        if (field[i][j][0] && !field[i][j+1][0])
         {
-          if (j != 19 && !field[i][j+1][0])
-          {
-            field[i][j+1][0] = 1;
-            field[i][j][0] = 0;
-            break;
-          }
+          field[i][j+1][0] = 1;
+          field[i][j][0] = 0;
         }
       }
     }
@@ -72,15 +60,12 @@ void Field::move(std::string str)
   {
     for (int i = 0; i < 16; i++)
     {
-      for (int j = 0; j < 20; j++)
+      for (int j = 1; j < 20; j++)
       {
-        if (field[i][j][0])
+        if (field[i][j][0] && !field[i][j-1][0])
         {
-          if (j != 0 && !field[i][j-1][0])
-          {
           field[i][j-1][0] = 1;
           field[i][j][0] = 0;
-          }
         }
       }
     }
